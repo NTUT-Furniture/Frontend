937caba40f6b9fc631a9b672ae7efda86333a051
garry911128 <garry80633@gmail.com>
2023-10-30 23:51:02 +0800

    complete naive bar and item card

---
+++ Sticky_Navbar.js
@@ -0,0 +1,10 @@
+window.onscroll = function() {myFunction()};
+var navbar = document.getElementById("navbar");
+
+function myFunction() {
+    if (window.scrollY > 0) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
