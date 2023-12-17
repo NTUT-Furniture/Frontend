5170fda01d63a777b7c5a455b31fd4a25bcb0904
CuChaos <t110590048@ntut.org.tw>
2023-12-17 23:17:07 +0800

    API for Sgin up

--- login/Login.js
+++ login/Login.js
@@ -7,34 +7,32 @@ function submitForm() {
     fetch('http://localhost:8000/api/token', {
         method: 'POST', // 修改为 POST 方法
         headers: {
-            'Accept': 'application/json', // 指定期望的响应格式
-            'Content-Type': 'application/x-www-form-urlencoded' // 设置内容类型
+            'Accept': 'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded'
         },
         body: new URLSearchParams({
             grant_type: '',
-            username: username, // 使用输入的用户名
-            password: password, // 使用输入的密码
+            username: username,
+            password: password,
             scope: '',
             client_id: '',
             client_secret: ''
-        }).toString() // 将数据转换为 x-www-form-urlencoded 格式
+        }).toString()
     })
-    .then(response => response.json()) // 处理 JSON 格式的响应
+    .then(response => response.json())
     .then(data => {
-        console.log(data);
-        UserLogin(data); // 调用 UserLogin 函数，传入响应数据和用户名密码
+        // console.log(data);
+        UserLogin(data);
     })
-    .catch(error => console.error('Error:', error)); // 处理错误
+    .catch(error => console.error('Error:', error));
 }
 function UserLogin(userData) {
     if (userData.detail==="Incorrect username or password") {
-        // 登录失败，给出提示
         alert('Invalid username or password. Please try again.');
     } 
-    if (userData.detail && userData.detail[0].type==="missing") {
+    else if (userData.detail && userData.detail[0].type==="missing") {
         alert('Missing username or password. Please try again.');
     }else {
-        // 登录成功，跳转到HomePage
         alert('Login successful! Redirecting to HomePage.');
         window.location.href = '../home/Index.html';
     }
