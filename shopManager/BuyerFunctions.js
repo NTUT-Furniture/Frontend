5f4c43bd25304a3c05f9938fd6ea6acddba851e0
garry911128 <garry80633@gmail.com>
2023-12-18 10:24:31 +0800

    try add api

--- shopManager/BuyerFunctions.js
+++ shopManager/BuyerFunctions.js
@@ -1,70 +1,61 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // 購物車資訊
-    const shoppingCart = [];
+    const managementContainer = document.getElementById('business-management');
+    let openDetailForm = null;
+    let currentDetailType = null;
 
-    // 獲取商品列表容器
-    const productContainer = document.getElementById('product-container');
-
-    // 獲取購物車顯示容器
-    const cartContainer = document.getElementById('cart-container');
-
-    // 獲取結帳按鈕
-    const checkoutButton = document.getElementById('checkout-button');
-
-    // 商品數據（模擬）
-    const products = [
-        {id: 1, name: 'Product A', price: 20},
-        {id: 2, name: 'Product B', price: 30},
-        {id: 3, name: 'Product C', price: 40},
-    ];
-
-    // 渲染商品列表
-    function renderProducts() {
-        products.forEach(product => {
-            const productElement = document.createElement('div');
-            productElement.innerHTML = `
-                <h3>${product.name}</h3>
-                <p>Price: $${product.price}</p>
-                <button class="add-to-cart-button" data-product-id="${product.id}">Add to Cart</button>
-            `;
-            productContainer.appendChild(productElement);
-
-            // 監聽添加到購物車按鈕點擊事件
-            const addToCartButton = productElement.querySelector('.add-to-cart-button');
-            addToCartButton.addEventListener('click', () => addToCart(product));
-        });
+    function renderBusinessCard(business) {
+        const card = document.createElement('div');
+        card.classList.add('business-card');
+        card.innerHTML = `
+            <h2 class="name">${business.name}</h2>
+            <p class="description">${business.description}</p>
+            <p class="quantity">Quantity: ${business.quantity}</p>
+            <p class="price">Price: ${business.price}</p>
+            <img class="image" src="${business.image_url}" alt="${business.name}">
+            <label for="quantity">Quantity:</label>
+            <input type="number" id="quantity" name="quantity" value="1" min="1">
+            <button class="buy-button">Buy</button>
+        `;
+        card.querySelector('.buy-button').addEventListener('click', () => handleBuyButtonClick(business, card));
+        managementContainer.appendChild(card);
     }
 
-    // 將商品添加到購物車
-    function addToCart(product) {
-        shoppingCart.push(product);
-        updateCartDisplay();
-    }
+    function handleBuyButtonClick(business, card) {
+        const quantityInput = card.querySelector('#quantity');
+        const quantity = parseInt(quantityInput.value, 10);
 
-    // 更新購物車顯示
-    function updateCartDisplay() {
-        cartContainer.innerHTML = '<h2>Shopping Cart</h2>';
-
-        if (shoppingCart.length === 0) {
-            cartContainer.innerHTML += '<p>Your cart is empty.</p>';
-        } else {
-            shoppingCart.forEach(item => {
-                const cartItemElement = document.createElement('div');
-                cartItemElement.innerHTML = `<p>${item.name} - $${item.price}</p>`;
-                cartContainer.appendChild(cartItemElement);
-            });
-
-            // 顯示結帳按鈕
-            cartContainer.appendChild(checkoutButton);
+        if (isNaN(quantity) || quantity <= 0) {
+            alert('Please enter a valid quantity.');
+            return;
         }
+
+        // 在這裡可以添加購買邏輯，使用 business 和 quantity 進行相應的處理
+        alert(`Buy ${quantity} of ${business.name}`);
     }
 
-    // 結帳按鈕點擊事件
-    checkoutButton.addEventListener('click', () => {
-        alert('Checkout functionality: Implement your checkout logic here.');
-    });
+    function fetchMockBusinessData() {
+        const mockData = [
+            {
+                id: 1,
+                name: 'Business A',
+                description: 'Description A',
+                quantity: 10,
+                price: 50,
+                image_url: '../Resources/bed.png',
+            },
+            {
+                id: 2,
+                name: 'Business B',
+                description: 'Description B',
+                quantity: 5,
+                price: 30,
+                image_url: '../Resources/chair.jpg',
+            },
+            // Add more mock data as needed
+        ];
+        mockData.forEach(renderBusinessCard);
+    }
 
-    // 初始化頁面
-    renderProducts();
-    updateCartDisplay();
+    fetchMockBusinessData();
+
 });
