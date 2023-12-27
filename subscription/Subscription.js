2df4c966001d21d814e1001fa778356be7ea4a4c
kyynk <zhan.kiliinn@gmail.com>
2023-12-27 21:37:37 +0800

    can return acc subscription result

--- subscription/Subscription.js
+++ subscription/Subscription.js
@@ -1,10 +1,24 @@
-function toggleSub() {
+async function toggleSub() {
     var subPopup = document.getElementById("sub-popup");
     subPopup.style.display = (subPopup.style.display === "block") ? "none" : "block";
+    let resultPromise = getSubscription();
+    let result = await resultPromise; // Wait for the promise to resolve
+    console.log('hihi');
+    console.log(result);
+
+    // Assuming the result is an object with a 'subscriptions' property
+    if (result && result.subscriptions) {
+        console.log('Subscriptions details:', result.subscriptions);
+        console.log('Subscriptions details:', result.subscriptions[0].shop_uuid);
+    } else {
+        console.log('No subscriptions found.');
+    }
 }
 
 async function getSubscription() {
+    console.log('Get Subscription');
     try {
+        sub = [];
         accID = getCookie('account_uuid');
         console.log('subscription account_uuid: ' + accID);
         const baseURL = `http://localhost:8000/api/subscription/account?`;
@@ -21,13 +35,14 @@ async function getSubscription() {
 
         if (response.ok) {
             const jsonResponse = await response.json();
+            console.log(jsonResponse);
             console.log('Success Get Subscription');
-            
+            return jsonResponse;
         } else {
             alert('u have not subscribe any shop');
+            return null;
         }
     } catch (error) {
         console.error('Get Subscription API Error', error);
     }
-    console.log('Get Subscription');
 }
