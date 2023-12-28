412afbd348cfd8f503e45159f6fd38daf98127c8
garry911128 <garry80633@gmail.com>
2023-12-28 12:51:56 +0800

    Revert "delete"
    This reverts commit bdb58d1060fbf4b1ce16b82645c04afa3a0b0644.

--- home/ItemcardComponent.js
+++ home/ItemcardComponent.js
@@ -26,6 +26,7 @@ class ItemCardComponent {
         })
         .catch(error => {
             console.error('Error:', error);
+            imageElement.src = "this.src='../Resources/defaultProduct.png'";
         });
         imageElement.alt = this.productName;
         imageElement.onerror = "this.src='../Resources/defaultProduct.png'";
