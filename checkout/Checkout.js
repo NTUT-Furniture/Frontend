19dfcf694c1ad5f7473b1e094037594ee521af58
kyynk <zhan.kiliinn@gmail.com>
2024-01-03 06:37:04 +0800

    use coupon

--- checkout/Checkout.js
+++ checkout/Checkout.js
@@ -54,12 +54,22 @@ async function applyCoupon() {
     let resultPromise = getCurrentCoupons();
     let nowCoupons = await resultPromise;
     console.log(nowCoupons);
+    let usingCoupon = false;
 
-    if (couponInput.value.trim() === 'SAVE10') {
-        message.textContent = 'Coupon applied successfully! Use Coupon: SAVE10';
-        displayShopping(0.1);
-    } else {
+    if (nowCoupons && nowCoupons.coupons) {
+        for (let index = 0; index < nowCoupons.coupons.length; index++) {
+            const element = nowCoupons.coupons[index];
+            if (couponInput.value.trim() === element.coupon_code) {
+                message.textContent = 'Coupon applied successfully! Use Coupon: ' + element.coupon_code;
+                displayShopping((100 - element.discount) / 100);
+                usingCoupon = true;
+                break;
+            }
+        }
+    }
+    if (! usingCoupon) {
         message.textContent = 'Invalid coupon code.';
+        displayShopping();
     }
 }
 
