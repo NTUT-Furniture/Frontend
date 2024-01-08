6fde601a5ce04f731ff77d6c6e60204623152429
DF <dragonflybaby2.0@gmail.com>
2024-01-09 05:23:21 +0800

    Remove the files as the backend docker have done the part

--- redirect.js
+++
@@ -1,6 +0,0 @@
-function redirectToPage() {
-    setTimeout(function() {
-    const baseURL = window.location.origin;
-        window.location.href = `${baseURL}/home/Index.html`; // Replace with the desired URL
-    },); // Redirect after 3 seconds (adjust the time as needed)
-}
