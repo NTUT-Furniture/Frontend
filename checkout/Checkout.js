568243197ddad285ccbec2abc61a1bf33d9a46de
kyynk <zhan.kiliinn@gmail.com>
2024-01-11 00:24:00 +0800

    rm some comment

--- checkout/Checkout.js
+++ checkout/Checkout.js
@@ -1,52 +1,36 @@
 let shopping = [];
 let usingCouponCode = "";
-// let allShop = [];
 
-// Display shopping cart content
 function displayShopping(coupon = 1, couponCode = "") {
-    // Retrieve the shopping cart data from the query parameters
     const urlParams = new URLSearchParams(window.location.search);
     const cartParam = urlParams.get('cart');
     if (cartParam) {
         try {
-            // Parse the JSON string to get the shopping cart array
             shopping = JSON.parse(decodeURIComponent(cartParam));
-            // allShop = [];
             usingCouponCode = couponCode;
         } catch (error) {
             console.error('Error parsing shopping cart data:', error);
         }
     }
 
-    // Perform checkout logic with the shopping cart data
     console.log('Checkout successful! Items purchased:', shopping);
     
-    var cartItem = document.getElementById('cartItem'); // Corrected ID here
+    var cartItem = document.getElementById('cartItem');
     cartItem.innerHTML = '';
-    // Add table headers
     var headerRow = cartItem.insertRow(0);
     headerRow.innerHTML = '<th>Product Name</th><th>Quantity</th><th>Price</th>';
 
     var cost = 0;
 
-    // Add table content
     shopping.forEach(function (item, index) {
-        var row = cartItem.insertRow(index + 1); // Index + 1 to skip the header row
+        var row = cartItem.insertRow(index + 1);
         row.innerHTML = `<td>${item.name}</td><td>${item.quantity}</td><td>${item.quantity * item.price}</td>`;
         cost += item.quantity * item.price;
-        // if (!allShop.includes(item.shopID)) {
-        //     allShop.push(item.shopID);
-        // }
     });
-    // console.log(allShop);
-    // console.log(cost);
-    // console.log(coupon);
-    // console.log(cost * coupon);
     const totalCost = document.getElementById('totalCost');
     totalCost.innerHTML = `<h2>total cost: ${cost * coupon}</h2>`;
 }
 
-// Apply coupon function
 async function applyCoupon() {
     var couponInput = document.getElementById('coupon');
     var message = document.getElementById('message');
@@ -94,11 +78,8 @@ async function getCurrentCoupons() {
 
         if (response.ok) {
             const jsonResponse = await response.json();
-            // console.log(jsonResponse);
-            // console.log('Success Get Current Coupons');
             return jsonResponse;
         } else {
-            // console.log('No coupons now');
             return null;
         }
     } catch (error) {
@@ -106,17 +87,14 @@ async function getCurrentCoupons() {
     }
 }
 
-// Checkout function
 async function checkout() {
     console.log('Checkout successful! Items purchased:', shopping);
     alert('Checkout successful!');
     let requestBodies = convertToRequestBody(shopping);
-    // console.log(JSON.stringify(requestBodies, null, 2));
 
     try {
         const transactionResults = await Promise.all(requestBodies.map(requestBody => createTransaction(requestBody)));
         
-        // Process transactionResults as needed
         transactionResults.forEach(result => {
             console.log(result);
         });
@@ -125,18 +103,15 @@ async function checkout() {
         window.location.href = '../home/Index.html';
     } catch (error) {
         console.error('Error during checkout:', error);
-        // Handle the error, e.g., display an error message to the user
         alert('Checkout failed. Please try again later.');
     }
 }
 
-// Continue shopping function
 function continueShopping() {
     window.location.href = '../home/Index.html';
 }
 
 function convertToRequestBody(productList) {
-    // Group items by shopID
     let groupedItems = {};
     productList.forEach(item => {
         if (!groupedItems[item.shopID]) {
@@ -145,7 +120,6 @@ function convertToRequestBody(productList) {
         groupedItems[item.shopID].push(item);
     });
 
-    // Create request bodies for each shopID
     let requestBodies = [];
     for (let shopID in groupedItems) {
         let products = {
@@ -177,12 +151,9 @@ function convertToRequestBody(productList) {
 }
 
 async function createTransaction(requestBody) {
-    // const baseURL = window.location.origin;
-
-    // Define the order of keys
     const keyOrder = ['shop_uuid', 'coupon_code', 'receive_time', 'status', 'products'];
     const sortedRequestBody = {};
-    // Sort the keys based on the defined order
+
     keyOrder.forEach(key => {
         if (requestBody.hasOwnProperty(key)) {
             sortedRequestBody[key] = requestBody[key];
@@ -214,11 +185,11 @@ async function createTransaction(requestBody) {
         console.log(`Request sent successfully for shop_uuid: ${requestBody.shop_uuid}`);
         console.log('API Response:', data);
 
-        return data; // Return data on success
+        return data;
     } catch (error) {
         console.error(`Error sending request for shop_uuid: ${requestBody.shop_uuid}`);
         console.error('Error details:', error);
-        throw error; // Throw the error to be caught by the calling function
+        throw error;
     }
 }
 
--- shoppingCart/ShoppingCart.js
+++ shoppingCart/ShoppingCart.js
@@ -1,52 +1,30 @@
 let shoppingCart = [];
 
-// // Function to get the shopping cart from local storage
-// function getShoppingCart() {
-//     const storedCart = localStorage.getItem('shoppingCart');
-//     return storedCart ? JSON.parse(storedCart) : [];
-// }
-
-// // Function to save the shopping cart to local storage
-// function saveShoppingCart(cart) {
-//     localStorage.setItem('shoppingCart', JSON.stringify(cart));
-// }
-
-// Function to save the shopping cart items in a cookie
 function saveShoppingCartToCookie() {
-    // Convert the shoppingCart array to a JSON string
     const cartString = JSON.stringify(shoppingCart);
     
-    // Set the cookie with the shopping cart data
     document.cookie = `shoppingCart=${cartString}; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/`;
 }
 
-// Function to load the shopping cart items from a cookie
 function loadShoppingCartFromCookie() {
-    // Retrieve the shopping cart data from the cookie
     const cartString = getCookie('shoppingCart');
 
     try {
-        // Check if the cartString is empty or null
         if (cartString && cartString.trim() !== '') {
-            // Attempt to parse the JSON string to get the shopping cart array
             shoppingCart = JSON.parse(cartString);
         } else {
-            // If cartString is empty or null, initialize shoppingCart with an empty array
             shoppingCart = [];
         }
     } catch (error) {
-        // Handle the error (e.g., log it or set shoppingCart to an empty array)
         console.error('Error parsing JSON data:', error);
         shoppingCart = [];
     }
 }
 
-// Function to clear the shopping cart cookie
 function clearShoppingCartCookie() {
     document.cookie = 'shoppingCart=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/';
 }
 
-// Helper function to get the value of a cookie by name
 function getCookie(cookieName) {
     const name = `${cookieName}=`;
     const decodedCookie = decodeURIComponent(document.cookie);
@@ -60,45 +38,30 @@ function getCookie(cookieName) {
     return '';
 }
 
-// Check if the page is one of the shared pages
 const sharedPages = ['/home/Index.html', '/shopManager/Shop.html', '/checkout/Checkout.html', '/productDetail/ProductDetail.html', /* Add other shared pages here */];
 const currentPage = window.location.pathname;
 
 if (sharedPages.includes(currentPage)) {
-    // Load the shopping cart from the cookie on shared pages
     window.addEventListener('load', loadShoppingCartFromCookie);
-
-    // // Add logic to clear the cookie on page refresh (F5)
-    // window.addEventListener('beforeunload', clearShoppingCartCookie);
 }
-
-// window.addEventListener('unload', function () {
-//     // Clear the shopping cart cookie when the user closes the browser tab or window
-//     clearShoppingCartCookie();
-// });
 
 function toggleCart() {
     const cartPopup = document.getElementById('cart-popup');
-    cartPopup.innerHTML = ''; // Clear previous content
+    cartPopup.innerHTML = '';
 
-    // console.log(shoppingCart);
     if (cartPopup.style.display === 'none' || cartPopup.style.display === '') {
-        // Show cart content
         cartPopup.style.display = 'block';
         showCartItems();
     } else {
-        // Hide cart content
         cartPopup.style.display = 'none';
     }
 }
 
-// Function to check if the user has an account token in the cookie
 function hasAccountToken() {
     var token = getCookie("token");
     return token !== null && token !== "";
 }
 
-// Function to get the value of a cookie by name
 function getCookie(name) {
     var cookies = document.cookie.split("; ");
     for (var i = 0; i < cookies.length; i++) {
@@ -110,35 +73,29 @@ function getCookie(name) {
     return null;
 }
 
-// Function to redirect to checkout page
 function redirectToCheckout() {
     if (hasAccountToken()){
-        // Implement the logic to navigate to the checkout page
         console.log('Redirecting to checkout page');
 
-        // Implement the logic to navigate to the checkout page
-        // Pass the shopping cart data to the checkout page using query parameters
         const queryString = `?cart=${encodeURIComponent(JSON.stringify(shoppingCart))}`;
-        const checkoutPageUrl = '../checkout/Checkout.html' + queryString; // Replace 'Checkout.html' with your actual checkout page URL
+        const checkoutPageUrl = '../checkout/Checkout.html' + queryString;
         window.location.href = checkoutPageUrl;
     }
     else {
-        // Redirect to the login page if there is no account token
         console.log('Redirecting to login page');
         alert('please login your account\nor create an new account to login')
-        window.location.href = '../login/Login.html'; // Replace with your actual login page URL
+        window.location.href = '../login/Login.html';
     }
 }
 
 function showCartItems() {
     const cartPopup = document.getElementById('cart-popup');
-    // Add a close button
     const closeButton = document.createElement('button');
     closeButton.innerText = 'Close';
     closeButton.onclick = toggleCart;
     closeButton.style.position = 'absolute';
-    closeButton.style.bottom = '10px';  // Adjust the bottom spacing as needed
-    closeButton.style.right = '10px';  // Adjust the right spacing as needed
+    closeButton.style.bottom = '10px';
+    closeButton.style.right = '10px';
     cartPopup.appendChild(closeButton);
 
     if (shoppingCart.length === 0) {
@@ -148,32 +105,16 @@ function showCartItems() {
     } else {
         const totalAmount = shoppingCart.reduce((sum, item) => sum + item.price * item.quantity, 0);
 
-        // shoppingCart.forEach(item => {
-        // const itemDiv = document.createElement('div');
-        // itemDiv.classList.add('item');
-        // itemDiv.innerHTML = `
-        //     <span>${item.name} (Qty: </span>
-        //     <input type="number" min="1" max="${item.stock}" value="${item.quantity}" onchange="updateQuantity('${item.id}', this.value)">
-        //     <span>)</span>
-        //     <div>$${item.price * item.quantity}</div>
-        //     <button onclick="removeItem('${item.id}')">Remove</button>
-        // `;
-        // cartPopup.appendChild(itemDiv);
-        // });
-        // Create a div element to wrap the shopping cart table
         const tableContainer = document.createElement('div');
         tableContainer.classList.add('shopping-cart-container');
-        // 創建一個<table>元素
+
         const table = document.createElement('table');
         table.classList.add('shopping-cart-table');
 
-        // 使用forEach遍歷shoppingCart中的每個商品項目
         shoppingCart.forEach(item => {
-            // 創建一個<tr>元素，表示一個商品項目
             const row = document.createElement('tr');
             row.classList.add('item-row');
 
-            // 使用<td>元素來呈現每個商品項目的不同部分
             const nameCell = document.createElement('td');
             nameCell.classList.add('item-cell');
             nameCell.textContent = item.name;
@@ -181,7 +122,7 @@ function showCartItems() {
 
             const quantityCell = document.createElement('td');
             quantityCell.classList.add('item-cell', 'qty-cell');
-            quantityCell.style.whiteSpace = 'nowrap';  // 防止內容換行
+            quantityCell.style.whiteSpace = 'nowrap';
             quantityCell.innerHTML = `
                 <span>(Qty: </span>
                 <input type="number" min="1" max="${item.stock}" value="${item.quantity}" onchange="updateQuantity('${item.id}', this.value)">
@@ -202,32 +143,27 @@ function showCartItems() {
             removeCell.appendChild(removeButton);
             row.appendChild(removeCell);
 
-            // 把這一行商品項目添加到表格中
             table.appendChild(row);            
         });
         
-        // Append the table to the container
         tableContainer.appendChild(table);
 
-        // Append the container to the cartPopup
         cartPopup.appendChild(tableContainer);
         
         const totalDiv = document.createElement('div');
         totalDiv.innerHTML = `<strong>Total:</strong> $${totalAmount}`;
         cartPopup.appendChild(totalDiv);
 
-        // Add a Checkout button
         const checkoutButton = document.createElement('button');
         checkoutButton.innerText = 'Checkout';
-        checkoutButton.onclick = redirectToCheckout; // Use your checkout function here
-        checkoutButton.style.marginTop = '10px'; // Add some spacing from the total
-        checkoutButton.classList.add('checkout-button'); // Optionally, add a class for styling
+        checkoutButton.onclick = redirectToCheckout;
+        checkoutButton.style.marginTop = '10px';
+        checkoutButton.classList.add('checkout-button');
         cartPopup.appendChild(checkoutButton);
     }
 }
 
 function addItemToCart(item) {
-    // console.log(item.id);
     const existingItem = shoppingCart.find(cartItem => cartItem.id === item.id);
 
     if (existingItem) {
@@ -240,15 +176,10 @@ function addItemToCart(item) {
     } else {
         shoppingCart.push({ ...item, quantity: 1 });
     }
-
-    // saveShoppingCart(shoppingCart);
     saveShoppingCartToCookie();
-    // console.log(shoppingCart);
-    // toggleCart();
 }
 
 function addItemToCartWithQuantity(item, num) {
-    // console.log(item.id);
     const existingItem = shoppingCart.find(cartItem => cartItem.id === item.id);
 
     if (existingItem) {
@@ -262,10 +193,7 @@ function addItemToCartWithQuantity(item, num) {
         shoppingCart.push({ ...item, quantity: num });
     }
 
-    // saveShoppingCart(shoppingCart);
     saveShoppingCartToCookie();
-    // console.log(shoppingCart);
-    // toggleCart();
 }
 
 function removeItem(itemId) {
@@ -275,34 +203,26 @@ function removeItem(itemId) {
         shoppingCart.splice(index, 1);
     }
 
-    // Clear the cartPopup and show the updated items
     const cartPopup = document.getElementById('cart-popup');
     cartPopup.innerHTML = '';
-    // saveShoppingCart(shoppingCart);
     saveShoppingCartToCookie();
     showCartItems();
 }
 
 function updateQuantity(itemId, newQuantity) {
-    // console.log(itemId);
-    // console.log(typeof(itemId));
-    // console.log(shoppingCart);
     console.log(`Updating quantity for item ${itemId} to ${newQuantity}`);
     const itemToUpdate = shoppingCart.find(item => item.id === itemId);
     const parsedQuantity = parseInt(newQuantity, 10);
 
     if (itemToUpdate && parsedQuantity >= 1) {
         itemToUpdate.quantity = parsedQuantity;
-        // saveShoppingCart(shoppingCart);
         saveShoppingCartToCookie();
     } else {
         console.error(`Item with ID ${itemId} not found in the shopping cart.`);
     }
 
-    // Clear the cartPopup and show the updated items
     const cartPopup = document.getElementById('cart-popup');
     cartPopup.innerHTML = '';
-    // saveShoppingCart(shoppingCart);
     saveShoppingCartToCookie();
     showCartItems();
 }
@@ -311,10 +231,3 @@ function testAddItem() {
     const item = {id: "1", name: 'Item 1', price: 50, stock: 5, shopID: "1"};
     addItemToCart(item);
 }
-
-// Example items
-// const item1 = { id: "5", name: 'Item 1', price: 20, stock: 5 };
-// const item2 = { id: "2", name: 'Item 2', price: 30, stock: 5 };
-
-// addItemToCart(item1);
-// addItemToCart(item2);
